d2c0de410c38f6d763419dc4e45617883ef7233d
Daniel Duggin <46377670+danielduggin2@users.noreply.github.com>
2024-02-02 19:11:32 -0600

    Take apart

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,6 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 
-import { UserView } from 'src/sections/user/view';
+// import { UserView } from 'src/sections/user/view';
 
 // ----------------------------------------------------------------------
 
@@ -11,7 +11,7 @@ export default function UserPage() {
         <title> User | Minimal UI </title>
       </Helmet>
 
-      <UserView />
+      {/* <UserView /> */}
     </>
   );
 }
